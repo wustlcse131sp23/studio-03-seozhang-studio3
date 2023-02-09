b68281910fb12a11e73afe22448b67a786a52a08
averyseo <averyseo@localhost>
2023-02-09 12:51:56 -0600

    Studio 3 - Avery and Junwei

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,20 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		
+Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter starting number to find prime numbers up to and including: ");
+		int n = in.nextInt();
+		
+		int[] array = new int[n];
+		
+		
+		
+	}
+
+}
